1bcc31d780b8eae66cf0d79cb2ae2a475bf2891b
Sourav Chakraborty <chakrabortysourav797@gmail.com>
2026-08-02 00:56:13 +0530

    completed_ci_cd.2

--- handler/routes.go
+++ handler/routes.go
@@ -40,6 +40,6 @@ func (h *Handler) RegisterRoutes(router *gin.Engine) {
 
 func (h *Handler) Health(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"status": "Hey, it's up and running",
+		"status": "Hey, it's up and running, Deployed via CI/CD",
 	})
 }
